5605a509dc63352ec4c6ef694e661b2e70fc417a
Santi-Ourthe <141781928+Santi-Ourthe@users.noreply.github.com>
2023-09-20 21:44:26 -0300

    base
    agrego carpeta base

---
+++ proyecto_base_generico/index.js
@@ -0,0 +1,24 @@
+
+
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
